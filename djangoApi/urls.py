82d1fbf102dc775c1720fde08555705982944829
seungwan <swsj4480@gmail.com>
2025-04-13 13:12:38 +0900

    fix : 기존의 restAPI 애플리케이션 모듈 삭제
    1. 장고 project에는 하위 디렉터리에 애플리케이션을 파줄 수 있음

--- djangoApi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# 이 코드는 Django 웹 애플리케이션의 URL 패턴을 정의하는 부분입니다.
-# 'urlpatterns' 리스트는 URL 경로와 해당 경로에 대한 뷰를 매핑합니다.
-# 여기서는 'hello/'라는 URL 경로가 'views.hello_world' 함수와 연결되어 있습니다.
-# 'name' 매개변수는 이 URL 패턴에 'hello_world'라는 이름을 부여하여,
-# 다른 곳에서 이 URL을 참조할 때 사용됩니다.
-urlpatterns = [path('hello/', views.hello_world, name='hello_world'), path('test/',views.test, name='test')]
